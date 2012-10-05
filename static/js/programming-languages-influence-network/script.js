9038b99695a94b34027b2dd216db0551cfb46e5f
Ramiro Gomez <web@ramiro.org>
2012-10-06 00:11:42 +0200

    programming languages v2 WIP

--- static/js/programming-languages-influence-network/script.js
+++ static/js/programming-languages-influence-network/script.js
@@ -1,4 +1,69 @@
+// langinfo must be accessible from the external freebase text service script
+var langinfo = function(data) {
+  var sl = $('#showlang');
+  sl.find('h3').text(hlang.label);
+  var influenced = '';
+  var influencedby = '';
+  var m = /\|/g;
+  $.each(hlang.attr.attributes, function(idx, item){
+    if (1 == item.attr) {
+      influencedby = item.val.replace(m, ', ');
+    }
+    if (2 == item.attr) {
+      influenced = item.val.replace(m, ', ');
+    }
+  });
+  var desc = data.result + '... <a href="http://www.freebase.com/view/' + hlang.id + '">view on Freebase</a>';
+
+  // in case of Ruby include Matz tweet
+  if ('Ruby' === hlang.label) {
+    desc = '<blockquote class="twitter-tweet"><p>NowBrowsing: Programming Languages Influence Network <a href="http://t.co/kzdSlrpt" title="http://exploringdata.github.com/vis/programming-languages-influence-network/">exploringdata.github.com/vis/programmin…</a></p>&mdash; Yukihiro Matsumoto (@yukihiro_matz) <a href="https://twitter.com/yukihiro_matz/status/251612155470823425" data-datetime="2012-09-28T09:19:47+00:00">September 28, 2012</a></blockquote><script src="//platform.twitter.com/widgets.js" charset="utf-8"></script>' + desc;
+  }
+
+  if (influenced)
+    desc += '<h4>Languages Influenced</h4><p>' + influenced + '</p>';
+  if (influenced)
+    desc += '<h4>Influenced by</h4><p>' + influencedby + '</p>';
+
+  sl.find('.modal-body').html('<div>' + desc + '<hr><p>Search for ' + hlang.label + ' books on <a href="http://www.amazon.com/gp/search?ie=UTF8&camp=1789&creative=9325&index=books&keywords=' + encodeURIComponent(hlang.label) + '&linkCode=ur2&tag=xpdt-20">Amazon.com</a></p></div>');
+  sl.modal();
+};
+
 $(function(){
   var G = visgexf.init('sig', '/gexf/plin_forceatlas2.gexf');
-  G.graphAttr();
+  var filterid = 0;
+  var filters = G.getFilters([filterid]);
+
+  var pmenu = $('#paradigms');
+  pmenu.append('<li class="active"><a href="#">All languages (' + G.sig.getNodesCount() + ')</a></li>');
+  $.each(filters, function(idx, item) {
+    pmenu.append('<li><a href="#' + item[0] + '">' + item[0] + ' (' + item[1] + ')</a></li>');
+  });
+  pmenu.click(function(e){
+    e.preventDefault();
+    if ('a' == e.target.nodeName.toLowerCase()) {
+      var t = $(e.target);
+      var pid = t.attr('href').replace('#', '');
+      pmenu.find('li').removeClass('active');
+      t.parent('li').addClass('active');
+      visgexf.setFilter(filterid, pid);
+    }
+  });
+
+  G.sig.bind('upnodes', function(event){
+    hlang = G.sig.getNodes(event.content)[0];
+    // add script with callback to avoid cross-origin request issues
+    var script = document.createElement('script');
+    script.src = 'https://usercontent.googleapis.com/freebase/v1/text' + hlang.id + '?callback=langinfo';
+    script.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(script);
+  });
+//.bind('outnodes',function(event){
+//    if (G.pid && G.pid != G.defaultPid) {
+//      G.hlParadigm(G.pid);
+//    } else {
+//      G.reset();
+//    }
+//  });
+
 });
--- static/js/visgexf.js
+++ static/js/visgexf.js
@@ -1,13 +1,18 @@
 var visgexf = {
   sig: null,
-  attr: {},
+  filters: {},
+  graph: null,
+  activeFilterId: null,
+  activeFilterVal: null,
+  sourceColor: '#67A9CF',
+  targetColor: '#EF8A62',
   init: function(visid, filename) {
     visgexf.sig = sigma.init(document.getElementById(visid)).drawingProperties({
       defaultLabelColor: '#fff',
       defaultLabelSize: 12,
       defaultLabelBGColor: '#fff',
       defaultLabelHoverColor: '#000',
-      labelThreshold: 4,
+      labelThreshold: 3,
       defaultEdgeType: 'curve'
     }).graphProperties({
       minNodeSize: 0.4,
@@ -25,21 +30,36 @@ var visgexf = {
       var hnode = visgexf.sig.getNodes(event.content)[0];
       if (0 == hnode.degree) return;
       var zoomratio = visgexf.sig.graphProperties().position().ratio;
-      var hlinks = {};
+      var sources = {},
+          targets = {};
       visgexf.sig.iterEdges(function(e){
         if (e.source != hnode.id && e.target != hnode.id) {
           e.hidden = 1;
         } else if (e.source == hnode.id) {
-          hlinks[e.target] = true;
+          targets[e.target] = true;
+          e.color = visgexf.sourceColor;
+        } else if (e.target == hnode.id) {
+          sources[e.source] = true;
+          e.color = visgexf.targetColor;
         }
       }).iterNodes(function(n){
-        if (n.id != hnode.id && !hlinks[n.id]) {
+        if (n.id == hnode.id) {
+          n.hidden = 0;
+        } else if (sources[n.id]) {
+          n.color = visgexf.targetColor;
+        } else if (targets[n.id]) {
+          n.color = visgexf.sourceColor;
+        }
+        else {
           n.hidden = 1;
         }
       }).draw(2,2,2);
     }).bind('outnodes',function(event){
       visgexf.sig.iterNodes(function(n){
         n.hidden = 0;
+        if (null !== visgexf.activeFilterId && null !== visgexf.activeFilterVal && !visgexf.nodeHasFilter(n, visgexf.activeFilterId, visgexf.activeFilterVal)) {
+          n.hidden = 1;
+        }
       }).iterEdges(function(e){
         e.hidden = 0;
       }).draw(2,2,2);
@@ -55,24 +75,54 @@ var visgexf = {
     return visgexf;
   },
 
-  graphAttr: function() {
+  // called with array of ids of attributes to use as filters
+  getFilters: function(attrids) {
     visgexf.sig.iterNodes(function(n) {
       n.attr.attributes.map(function(node) {
-        if(!Number.isNaN(parseInt(node.attr))) {
-          var val = node.val;
-          if (!visgexf.attr.hasOwnProperty(val)) {
-            visgexf.attr[val] = 0;
+        if (-1 !== attrids.indexOf(parseInt(node.attr))) {
+          var vals = node.val.split('|');
+          for (v in vals) {
+            val = vals[v];
+            if (!visgexf.filters.hasOwnProperty(val)) {
+              visgexf.filters[val] = 0;
+            }
+            visgexf.filters[val]++;
           }
-          visgexf.attr[val]++;
         }
       });
     });
-
+    // sort by frequencies of filter attributes
     var sorted = [];
-    for (var a in visgexf.attr) {
-      sorted.push([a, visgexf.attr[a]]);
+    for (var a in visgexf.filters) {
+      sorted.push([a, visgexf.filters[a]]);
     }
     sorted.sort(function(a, b) { return b[1] - a[1] });
     return sorted;
+  },
+
+  nodeHasFilter: function(node, filterid, filterval) {
+    var hasFilter = false;
+    for (i in node.attr.attributes) {
+      var item = node.attr.attributes[i];
+      if (filterid === parseInt(item.attr)) {
+        if (-1 !== item.val.indexOf(filterval)) {
+          hasFilter = true;
+          break;
+        }
+      }
+    }
+    return hasFilter;
+  },
+
+  // show only nodes that match filter
+  setFilter: function(filterid, filterval) {
+    visgexf.activeFilterId = filterid;
+    visgexf.activeFilterVal = filterval;
+    visgexf.sig.iterNodes(function(n){
+      n.hidden = filterval ? 1 : 0;
+      if (visgexf.nodeHasFilter(n, filterid, filterval)) {
+        n.hidden = 0;
+      }
+    }).draw(2,2,2);
   }
 };
