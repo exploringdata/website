00df4c5b39ea41dd411d91bf6ef1972cf3f56776
Ramiro Gomez <web@ramiro.org>
2012-10-16 14:46:12 +0200

    james bond actors network

--- static/js/james-bond-actors-network/script.js
+++ static/js/james-bond-actors-network/script.js
@@ -1,9 +1,15 @@
-// langinfo must be accessible from the external freebase text service script
-var langinfo = function(data) {
+// nodeinfo must be accessible from the external freebase text service script
+var nodeinfo = function(data) {
   var sl = $('#showlang');
-  sl.find('h3').text(hlang.label);
+  sl.find('h3').text(hnode.label);
 
-  var desc = data.result + '... <a href="http://www.freebase.com/view/' + hlang.id + '">view on Freebase</a>';
+  var desc = '<img src="https://usercontent.googleapis.com/freebase/v1/image' + hnode.id + '?maxwidth=200&maxheight=150" class="img-polaroid pull-right" alt="photo">';
+  $.each(hnode.attr.attributes, function(idx, item) {
+    if(0 == item.attr) {
+      desc += '<h4>James Bond films starred</h4><p>' + item.val.split('|').sort().join(', ') + '</p>';
+    }
+  });
+  desc += '<h4>Bio</h4>' + data.result + '... <a href="http://www.freebase.com/view/' + hnode.id + '">view on Freebase</a>';
 
   sl.find('.modal-body').html(desc);
   sl.modal();
@@ -22,10 +28,10 @@ var menuclick = function(menu, event) {
 
 var nodeClick = function(Graph) {
   Graph.sig.bind('upnodes', function(event){
-    hlang = Graph.sig.getNodes(event.content)[0];
+    hnode = Graph.sig.getNodes(event.content)[0];
     // add script with callback to avoid cross-origin request issues
     var script = document.createElement('script');
-    script.src = 'https://usercontent.googleapis.com/freebase/v1/text' + hlang.id + '?callback=langinfo';
+    script.src = 'https://usercontent.googleapis.com/freebase/v1/text' + hnode.id + '?callback=nodeinfo';
     script.type = 'text/javascript';
     document.getElementsByTagName('head')[0].appendChild(script);
   });
@@ -38,7 +44,8 @@ $(function(){
       defaultLabelSize: 12,
       defaultLabelBGColor: '#fff',
       defaultLabelHoverColor: '#000',
-      labelThreshold: 3,
+      labelThreshold: 2,
+      defaultEdgeType: 'curve'
     },
     graph: {
       minNodeSize: 1,
@@ -50,13 +57,13 @@ $(function(){
     type: 'undirected'
   }
 
-  var G = visgexf.init('sig', '/gexf/jamesbond.gexf', props);
+  var G = visgexf.init('sig', '/gexf/jamesbond_fr.gexf', props);
   var filterid = 0;
   var filters = G.getFilters([filterid]);
   nodeClick(G);
 
   var pmenu = $('#paradigms');
-  pmenu.append('<li class="active"><a href="#">All languages (' + G.sig.getNodesCount() + ')</a></li>');
+  pmenu.append('<li class="active"><a href="#">All Films (' + G.sig.getNodesCount() + ')</a></li>');
   $.each(filters, function(idx, item) {
     pmenu.append('<li><a href="#' + item[0] + '">' + item[0] + ' (' + item[1] + ')</a></li>');
   });
