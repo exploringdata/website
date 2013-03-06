b2848d386658e585a2934d02a95bb16310bf780a
Ramiro Gómez <web@ramiro.org>
2013-03-06 01:33:14 +0100

    listen on hashchange in sigma graphs, programmers search network

--- static/js/visgexf.js
+++ static/js/visgexf.js
@@ -113,7 +113,7 @@ var visgexf = {
   },
 
   highlightNode: function(node) {
-    var zoomratio = visgexf.sig.graphProperties().position().ratio;
+    //visgexf.sig.zoomTo(node.displayX, node.displayY, 5);
     var sources = {},
         targets = {};
     visgexf.sig.iterEdges(function(e){
@@ -154,15 +154,26 @@ var visgexf = {
       if ('' == $(event.target).val().trim())
         visgexf.resetSearch();
     });
-    if (document.location.hash) {
+    if (document.location.hash)
       visgexf.nodeSearch(document.location.hash.replace(/^#/, ''));
-    }
+
+    $(window).bind('hashchange', function(event) {
+      $('.modal').modal('hide');
+      visgexf.nodeSearch(document.location.hash.replace(/^#/, ''));
+    });
+    $('#search-reset').on('click', function(event) {
+      visgexf.resetSearch();
+    });
+  },
+
+  queryHasResult: function(q) {
+    return -1 !== visgexf.nodelabels.indexOf(q);
   },
 
   nodeSearch: function(query) {
-    document.location.hash = query;
     visgexf.resetFilter();
-    if (node = visgexf.sig.getNodes(visgexf.nodemap[query])) {
+    if (visgexf.queryHasResult(query)) {
+      node = visgexf.sig.getNodes(visgexf.nodemap[query])
       visgexf.highlightNode(node);
       return query;
     }
@@ -187,7 +198,8 @@ var visgexf = {
   },
 
   resetSearch: function() {
-    visgexf.searchInput.attr('value', '');
+    document.location.hash = '';
+    visgexf.searchInput.val('');
     visgexf.resetNodes();
   },
 
@@ -202,7 +214,7 @@ var visgexf = {
   reset: function() {
     visgexf.activeFilterId = null;
     visgexf.activeFilterVal = null;
-    visgexf.searchInput.attr('value', '');
+    visgexf.searchInput.val('');
     $('.graphfilter li').removeClass('active');
     visgexf.resetNodes();
   }
