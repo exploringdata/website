6ec1aacf05a3bc06e24ab6ca1803a267ec8861c1
Ramiro Gómez <web@ramiro.org>
2013-03-28 20:55:11 +0100

    consistent chart UI, JS code cleanup

--- static/js/climate-changes-decade/script.js
+++ static/js/climate-changes-decade/script.js
@@ -1,3 +1,6 @@
+var colors = d3.scale.category20();
+keyColor = function(d, i) {return colors(d.key)};
+
 function getPrevDay(date) {
     return new Date((new Date(date).getTime() / 1000 - 86400) * 1000)
 }
@@ -18,43 +21,44 @@ function getGuardianArticles(query, from, to) {
     });
 }
 
-function historyStream(selector, data) {
-    var colors = d3.scale.category20();
-    keyColor = function(d, i) {return colors(d.key)};
-
-    nv.addGraph(function() {
-        var chart = nv.models.stackedAreaChart()
-            .x(function(d) { return d[0] })
-            .y(function(d) { return d[1] })
-            .color(keyColor);
-
-        chart.xAxis
-            .showMaxMin(false)
-            .tickFormat(function(d) { return d3.time.format('%x')(new Date(d)) });
-
-        chart.yAxis
-            .tickFormat(d3.format(',d'));
-
-        d3.select(selector).append('div').attr('class', 'history').append('svg')
-            .datum(data)
-          .transition().duration(500).call(chart);
-
-        nv.utils.windowResize(chart.update);
-        return chart;
+function getHistoryData(json) {
+    return json.map(function(query){
+        values = query.values.map(function(val){
+            return {x:val[0], y:val[1]}
+        });
+        return {'key': query['key'], 'values': values};
     });
 }
 
-function historyMultiBar(selector, data) {
-    nv.addGraph(function() {
-        var data2 = [];
-        data.map(function(page){
-            values = page.values.map(function(val){
-                return {x:val[0], y:val[1]}
-            });
-            data2.push({'key': page['key'], 'values': values});
-        });
+function barValues(freqdata) {
+    return freqdata.slice(0, 20).map(function(item){
+        return {label: item[0], value: item[1]}
+    });
+}
 
-        var chart = nv.models.multiBarChart().stacked(true);
+// FIXME click doesn't work after nv.utils.windowResize(chart.update)
+function historyClick(hist, json) {
+    hist.selectAll('rect').on('click', function(d, i) {
+        var label = json[d.series].key;
+        // milliseconds need to be converted back to seconds
+        var file = label.replace(' ', '-') + '/' + d.x / 1000 + '.json';
+        d3.json('/json/climate-changes-decade/' + file, function(freqdata) {
+            barChart('#words', [{
+                key: label,
+                values: barValues(freqdata.words)
+            }]);
+            barChart('#sections', [{
+                key: label,
+                values: barValues(freqdata.sections)
+            }]);
+        });
+    });
+}
+
+function historyMultiBar(selector, json) {
+    nv.addGraph(function() {
+        var data = getHistoryData(json);
+        var chart = nv.models.multiBarChart().stacked(true).color(keyColor);
 
         chart.xAxis
             .showMaxMin(false)
@@ -64,46 +68,29 @@ function historyMultiBar(selector, data) {
             .tickFormat(d3.format(',d'));
 
         var hist = d3.select(selector).append('div').attr('class', 'history').append('svg');
-        hist.datum(data2)
+        hist.datum(data)
             .transition().duration(500).call(chart);
 
-        // FIXME click doesn't work after nv.utils.windowResize(chart.update)
-        hist.selectAll('rect').on('click', function(d, i) {
-            var label = data[d.series].key;
-            var file = label.replace(' ', '-') + '/' + d.x/1000 + '.json';
-            d3.json('/json/climate-changes-decade/' + file, function(json) {
-                barChart('#words', [{
-                    key: label,
-                    values: json.words.slice(0, 20).map(function(item){
-                        return {label: item[0], value: item[1]}
-                    })
-                }]);
-                barChart('#sections', [{
-                    key: label,
-                    values: json.sections.slice(0, 20).map(function(item){
-                        return {label: item[0], value: item[1]}
-                    })
-                }]);
-            });
-        });
-
         nv.utils.windowResize(chart.update);
+        historyClick(hist, json);
         return chart;
     });
 }
 
+// FIXME add click handler to search for specific articles
 function barChart(selector, data) {
     nv.addGraph(function() {
       var chart = nv.models.multiBarHorizontalChart()
           .x(function(d) { return d.label })
           .y(function(d) { return d.value })
           .margin({top: 30, right: 20, bottom: 50, left: 120})
-          .showValues(true)
-          .tooltips(false)
-          .showControls(false);
+          .showValues(false)
+          .tooltips(true)
+          .showControls(false)
+          .color(keyColor);
 
       chart.yAxis
-          .tickFormat(d3.format(',.2f'));
+          .tickFormat(d3.format(',d'));
 
       d3.select(selector).selectAll('div.bar').remove();
       d3.select(selector).append('div').attr('class', 'bar').append('svg')
