f88e03041aa08d6e56af2f6b9f1f0e64b161714e
Ramiro Gómez <code@ramiro.org>
2014-06-11 21:10:43 +0200

    #5: migrate to twitter typeahead.js WIP

--- static/js/climate-changes-decade/script.js
+++ static/js/climate-changes-decade/script.js
@@ -101,7 +101,7 @@ function getGuardianArticles(query, date) {
             for (i in data.response.results) {
                 var r = data.response.results[i];
                 var src = 'undefined' !== typeof r.fields.img-thumbnail ? r.fields.img-thumbnail : '/img/no-image.70x42.png';
-                html += '<div class="article row"><div class="image col-md-1"><a href="' + r.webUrl + '"><img src="' + src + '"></a></div><div class="col-md-5"><div title="' + r.webTitle + '"><a href="' + r.webUrl + '">' + r.fields.headline + '</a></div><span class="meta"><i class="glyphicon glyphicon-calendar"></i> ' + new Date(r.webPublicationDate).toGMTString() + ' in ' + r.sectionName + '</span></div></div><hr>';
+                html += '<div class="article row"><div class="image col-md-1"><a href="' + r.webUrl + '"><img src="' + src + '"></a></div><div class="col-md-5"><div title="' + r.webTitle + '"><a href="' + r.webUrl + '">' + r.fields.headline + '</a></div><span class="meta"><i class="fa fa-calendar"></i> ' + new Date(r.webPublicationDate).toGMTString() + ' in ' + r.sectionName + '</span></div></div><hr>';
             }
             html += getArticlePager(data);
             d3.select('#articles').html(html);
