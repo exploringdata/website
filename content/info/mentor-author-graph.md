6ed5216910b2bc567d5c833bf36f594532f8d105
Ramiro Gómez <code@ramiro.org>
2018-11-14 23:13:18 +0100

    Use dataTables library Create several links in book links column Update jQuery

--- content/info/mentor-author-graph.md
+++ content/info/mentor-author-graph.md
@@ -7,8 +7,11 @@ template: page-wide.html
 image: mentor-author-graph.png
 tags: [network graph, books, authors, sigmajs, gephi, networkx, interactive]
 scripts:
-- /vendor/tablesort.min.js
+- /vendor/npm/jquery.dataTables.min.js
 - /compiled/mentor-author-graph-info.js
+styles:
+
+- //cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css
 ---
 
 A network graph of authors, books and mentors based on recommendations in the book Tribe Of Mentors by Tim Ferriss and his interviewees.
@@ -18,10 +21,14 @@ let authors = 0;
 let books = 0;
 let mentors = 0;
 
-<table id="books">
-<tr data-sort-method="none">
-    <th>Author</th><th>Title</th><th data-sort-default>Recommended by</th><th>Buy book</th>
+<table id="books" class="display">
+<thead>
+<tr>
+    <th>Author</th><th>Title</th><th>Recommended by</th><th>Book links</th>
 </tr>
+</thead>
+<tbody>
+</tbody>
 </table>
 
-<div id="amzn-assoc-ad-623d5c40-6d6c-484c-8c0d-21afa99a74c2"></div><script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=623d5c40-6d6c-484c-8c0d-21afa99a74c2"></script>
+<!-- <div id="amzn-assoc-ad-623d5c40-6d6c-484c-8c0d-21afa99a74c2"></div><script async src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=623d5c40-6d6c-484c-8c0d-21afa99a74c2"></script> -->
--- src/js/mentor-author-graph-info.js
+++ src/js/mentor-author-graph-info.js
@@ -1,13 +1,4 @@
-// visgexf.sig.iterNodes(n => {
-//     let attr = n.attr.attributes;
-//     if (attr.author) authors++;
-//     if (attr.book) books++;
-//     if (attr.mentor) mentors++;
-// });
-
-
 jQuery.getJSON('/gexf/mentor-author-graph.json', data => {
-    let books = [];
     let table = jQuery('#books');
     for (let node of data.nodes) {
         if (node.attributes.hasOwnProperty('book')) {
@@ -20,15 +11,30 @@ jQuery.getJSON('/gexf/mentor-author-graph.json', data => {
                     mentors.push(edge.sourceID);
                 }
             }
+            let mentor_links = mentors.map(d => `<a href="/vis/mentor-author-graph/#${d}">${d}</a>`);
+
+            let book_links = [];
+            if (node.attributes.hasOwnProperty('asin')) {
+                book_links.push(`<a href="${node.attributes.url}">Amazon.com</a>`);
+                book_links.push(`<a href="https://openlibrary.org/search?isbn=${node.attributes.asin}&mode=everything">Open Library</a>`);
+            } else {
+                book_links.push(`<a href="https://www.amazon.com/gp/search?ie=UTF8&tag=xpdt-20&index=books&keywords=${encodeURIComponent(node.attributes.title)}">Amazon.com</a>`);
+                book_links.push(`<a href="https://openlibrary.org/search?q=${encodeURIComponent(node.attributes.title)}&mode=everything">Open Library</a>`);
+            }
+            book_links.push(`<a href="https://www.overdrive.com/search?q=${encodeURIComponent(node.attributes.title)}">Overdrive</a>`);
+
             table.append(`<tr>
-                <td>${author}</td>
-                <td>${node.id}</td>
-                <td data-sort="${mentors.length}">${mentors.join(', ')}</td>
-                <td><a href="${node.attributes.url}">Amazon.com</a></td>
+                <td data-order="${author}"><a href="/vis/mentor-author-graph/#${author}">${author}</a></td>
+                <td data-order="${node.attributes.title}"><a href="/vis/mentor-author-graph/#${node.attributes.title}">${node.attributes.title}</a></td>
+                <td data-order="${mentors.length},${mentors}">${mentor_links.join(' - ')}</td>
+                <td>${book_links.join(' - ')}</td>
             </tr>`)
-            books.push(node);
         }
     }
-
-    new Tablesort(document.getElementById('books'), {descending: true});
+    table.DataTable({
+        columnDefs: [{ 'orderable': false, 'targets': 3 }],
+        info: false,
+        order: [[2, 'desc']],
+        paging: false
+    });
 })
