22a926d699535c4cec7e7b6d7d079171aacb0dff
Ramiro Gómez <code@ramiro.org>
2020-11-18 00:24:33 +0100

    Change deploy source to public

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ deploy_repo=~/repos/deploy/exploringdata.github.com
 
 logya gen --keep
 
-rsync -aruvz --exclude=.git --exclude=.gitignore --delete deploy/ "$deploy_repo"
+rsync -aruvz --exclude=.git --exclude=.gitignore --delete public/ "$deploy_repo"
 
 cd "$deploy_repo"
 git add .
